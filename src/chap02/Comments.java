281d62f2d6cd8e105cd51b45c732f4489556c039
sjpark-dev <sjpark.dev@gmail.com>
2020-11-20 20:24:05 +0900

    added Comments.java

---
+++ src/chap02/Comments.java
@@ -0,0 +1,14 @@
+package chap02;
+
+import java.util.List;
+import java.util.Map;
+
+public class Comments {
+    public static void main(String[] args) {
+        // line comment
+
+        /*
+        block comment
+         */
+    }
+}
