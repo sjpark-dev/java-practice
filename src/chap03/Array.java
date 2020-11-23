104b701ecf1e52d118de11da5c395ee2b7d21449
sjpark-dev <sjpark.dev@gmail.com>
2020-11-23 16:30:36 +0900

    added Array.java

---
+++ src/chap03/Array.java
@@ -0,0 +1,24 @@
+package chap03;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] evens = {2, 4, 6, 8, 10};
+
+        String[] week = new String[7];
+        week[0] = "Mon";
+        week[1] = "Tue";
+        week[2] = "Wed";
+        week[3] = "Thu";
+        week[4] = "Fri";
+        week[5] = "Sat";
+        week[6] = "Sun";
+
+        System.out.println(week[6]); // Sun
+
+        // length
+        String[] fruits = {"apple", "orange", "grape", "strawberry", "watermelon", "kiwi"};
+        for (int i=0; i<fruits.length; i++) {
+            System.out.print(fruits[i] + " "); // apple orange grape strawberry watermelon kiwi
+        }
+    }
+}
