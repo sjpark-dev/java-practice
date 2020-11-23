c0547596628b69c27225f2989b90a38832f4f8d5
sjpark-dev <sjpark.dev@gmail.com>
2020-11-23 16:20:14 +0900

    added StringBufferType.java

---
+++ src/chap03/StringBufferType.java
@@ -0,0 +1,23 @@
+package chap03;
+
+public class StringBufferType {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        System.out.println(sb.toString()); // Hello
+
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("Hello");
+        sb1.append(" ");
+        sb1.append("World");
+        System.out.println(sb1.toString()); // Hello World
+
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("World");
+        sb2.insert(0, "Hello ");
+        System.out.println(sb2.toString()); // Hello World
+
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Hello World");
+        System.out.println(sb3.substring(0, 5)); // Hello
+    }
+}
