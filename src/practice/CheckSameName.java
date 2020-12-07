99831e38ad41e6158ac79bba1dbd050398a5c435
sjpark-dev <sjpark.dev@gmail.com>
2020-12-07 12:56:16 +0900

    update CheckSameName.java

--- src/practice/CheckSameName.java
+++ src/practice/CheckSameName.java
@@ -1,22 +1,33 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import com.sun.xml.internal.ws.util.QNameMap;
+
+import java.util.*;
 
 public class CheckSameName {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] namesInStringArray = scanner.nextLine().split(" ");
+
+        // 방법 1
         ArrayList<String> namesInArrayList = new ArrayList<>(Arrays.asList(namesInStringArray));
         int length = namesInArrayList.size();
 
         for (int i = 0; i < length; i++) {
             String name = namesInArrayList.remove(i);
             if (namesInArrayList.contains(name)) {
-                System.out.println("same name exits");
+                System.out.println("same name exists");
             }
             namesInArrayList.add(i, name);
+        }
+
+        // 방법 2
+        Set<String> namesSet = new HashSet<>(Arrays.asList(namesInStringArray));
+
+        if (namesInStringArray.length == namesSet.size()) {
+            System.out.println("Unique");
+        } else {
+            System.out.println("same name exists");
         }
     }
 }
