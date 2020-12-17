7e9b1903f567c8f52b76e1f91a429f90a7cdabf4
sjpark-dev <sjpark.dev@gmail.com>
2020-12-17 15:45:19 +0900

    Update EnumPractice.java

--- src/practice/enumpractice/EnumPractice.java
+++ src/practice/enumpractice/EnumPractice.java
@@ -2,11 +2,23 @@ package practice.enumpractice;
 
 public class EnumPractice {
     public static void main(String[] args) {
+        // values() 메쇼드
         for (Menu m : Menu.values()) {
             System.out.println(m.getCost());
         }
 
+        // valueOf() 메소드
         System.out.println(Payment.valueOf("NAVERPAY").getPaymentCode().getPaymentCode());
         System.out.println(Payment.NAVERPAY.getPaymentCode().getPaymentCode());
+
+        // ordinal() 메소드
+        System.out.println(Numbers.ONE.ordinal());
+        System.out.println(Numbers.TWO.ordinal());
+        System.out.println(Numbers.ZERO.ordinal());
+
+        // name() 메소드
+        System.out.println(Numbers.ONE.name());
+        System.out.println(Numbers.TWO.name());
+        System.out.println(Numbers.ZERO.name());
     }
 }
